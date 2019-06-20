7626b88086d499ac50a782aca56910f52a66c570
nikx1015 <nikx1015@gmail.com>
2019-06-20 14:37:23 -0400

    database migrations

---
+++ Bangazon/Migrations/20190620182209_payment-type-update.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bangazon.Migrations
+{
+    public partial class paymenttypeupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "00000000-ffff-ffff-ffff-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "4d49bb88-515f-4668-860d-beb1cf4b6244", "AQAAAAEAACcQAAAAEMxalM9zsqo8J922QTU7wSL4I4NZX9Yg6Mf9ZWxLoVN/YwUvrtLXM+UebVGSEs/63Q==" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "00000000-ffff-ffff-ffff-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "95614617-ebe7-42c1-a2d7-bfefc928c102", "AQAAAAEAACcQAAAAEMd1aKZOexC5HF+4qT6hIXFx465XNRiZYvmocf/NdOu/KgdGpSzkBtvxRFahGJqXBA==" });
+        }
+    }
+}
